f842ec62ca96fa730c7b9bea25ce30187d65e5b2
nurulafsar02 <khiladimoshin@gmail.com>
2022-11-15 23:17:24 +0600

    login and firebase add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,35 +1,34 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import loginImg from '../../assets/images/login-singin side img.png';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const handleLogin = data => {
+        console.log(data);
+    }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col ">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <Link to='singin' className="label-text-alt link link-hover">Forgot password?</Link>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
+        <div className='h-[450px] mt-10 mb-20 grid lg:grid-cols-2 grid-cols-1 '>
+            <div className='w-96 lg:ml-52 px-8 pb-8 m-0 shadow-2xl rounded-2xl'>
+                <h2 className='text-2xl font-bold my-5 text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="email" {...register("email")} className="input input-bordered w-full " required />
                     </div>
-                </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full " required />
+                    </div>
+                    <input className='btn bg-gradient-to-r from-cyan-500 to-green-400 hover:from-green-500 hover:to-pink-500 w-full mt-5' value='login' type="submit" />
+                </form>
+                <p className='my-2'>New to here <Link to='/singin' className='text-green-500 '>Create New account</Link> </p>
+                <div className='divider' >OR</div>
+                <button className='btn btn-outline w-full'>CONTINEW WITH GOOGLE</button>
+            </div>
+            <div>
+                <img src={loginImg} className='h-[450px]' alt="" />
             </div>
         </div>
     );
--- src/Pages/SingIn/Singin.js
+++ src/Pages/SingIn/Singin.js
@@ -1,38 +1,36 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import loginImg from '../../assets/images/login-singin side img.png';
 
 const Singin = () => {
+    const { register, handleSubmit } = useForm();
+    const handleSingin = data => {
+        console.log(data);
+    }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col ">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Singin now!</h1>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <Link to='singin' className="label-text-alt link link-hover">Forgot password?</Link>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
+        <div className='h-[450px] mt-10 mb-20 grid lg:grid-cols-2 grid-cols-1 '>
+            <div className='w-96 lg:ml-52 px-8 pb-8 m-0 shadow-2xl rounded-2xl'>
+                <h2 className='text-2xl font-bold my-5 text-center'>Singin</h2>
+                <form onSubmit={handleSubmit(handleSingin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="email" {...register("email")} className="input input-bordered w-full " required />
                     </div>
-                </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full " required />
+                    </div>
+                    <input className='btn bg-gradient-to-r from-cyan-500 to-green-400 hover:from-green-500 hover:to-pink-500 w-full mt-5' value='Singin' type="submit" />
+                </form>
+                <p className='my-2'>Forget pssword: <Link to='/singin' className='text-green-500 '> Reset Password</Link> </p>
+                <div className='divider' >OR</div>
+                <button className='btn btn-outline w-full'>CONTINEW WITH GOOGLE</button>
+            </div>
+            <div>
+                <img src={loginImg} className='h-[450px]' alt="" />
             </div>
         </div>
     );
-};
-
+}
 export default Singin;
